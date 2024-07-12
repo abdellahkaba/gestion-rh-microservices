4769ff73ab66e28bf7986ddde0b770f9db162dbb
Abdellah <abdallahkaba98@gmail.com>
2024-07-12 10:19:11 +0000

    configuration-v1

---
+++ services/config-server/src/main/java/com/isi/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.isi.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
