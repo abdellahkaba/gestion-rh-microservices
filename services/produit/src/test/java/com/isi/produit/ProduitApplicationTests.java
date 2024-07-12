2567de6f38d35180aef78cb177b9d8ac0084a39e
Abdellah <abdallahkaba98@gmail.com>
2024-07-12 17:55:19 +0000

    configuration-database-produit

---
+++ services/produit/src/test/java/com/isi/produit/ProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isi.produit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
